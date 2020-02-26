6414f9b436b003f9a2e1bb59c991b338edc1d066
noobres00 <61514133+noobres00@users.noreply.github.com>
2020-02-26 14:18:16 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -11,7 +11,11 @@ janela.geometry('500x500')
 texto1 = Label(text='primeiro texto')
 texto1.pack()
 
-botao = Button(text='botao 01', command = teste_botao).pack()
+texto2 = Label(text='segundo texto')
+texto2.pack()
+
+botao1 = Button(text='botao 01', command = teste_botao).pack()
+botao2 = Button(text='botao 02', command = teste_botao).pack()
 
 
 janela.mainloop()
