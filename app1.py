dfbd3098a3f5b4abb9f747005f4a79d44b9a4ba6
Denis Nobre <denis@denisnobre.com>
2020-02-27 06:27:38 -0300

    adicionado exemplo 02

--- app1.py
+++ app1.py
@@ -17,5 +17,18 @@ texto2.pack()
 botao1 = Button(text='botao 01', command = teste_botao).pack()
 botao2 = Button(text='botao 02', command = teste_botao).pack()
 
+listbox = Listbox(janela)
+
+listbox.insert(1,"India")  
+  
+listbox.insert(2, "USA")  
+  
+listbox.insert(3, "Japan")  
+  
+listbox.insert(4, "Austrelia")  
+  
+listbox.pack()  
+
+
 
 janela.mainloop()
