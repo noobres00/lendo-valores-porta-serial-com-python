02be9632ed1a5f80fcb48669256088cf851220f0
Denis Nobre <denis@denisnobre.com>
2020-03-02 07:31:25 -0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,10 +1,7 @@
-# -*- coding: cp1252 -*-
+
 import serial.tools.list_ports
 import tkinter as tk
 from tkinter import ttk
-
-#class app(tk.Tk()
-
 
 def selec_port():
     
@@ -18,15 +15,13 @@ for element in comlist:
     connected.append(element.device)
 
 
-
-
 # caracateristicas da janela
 janela = tk.Tk()
 janela.title('Teste Serial')
 janela.geometry('500x300+100+100')
 
 # textos
-txt_lista_portas = tk.Label(janela, text='Portas Disponíveis: ')
+txt_lista_portas = tk.Label(janela, text='Portas Disponiveis: ')
 txt_lista_portas.grid(column=0, row=1)
 
 txt_porta_selecionada_opt = tk.Label(janela, text='Porta Selecionada: ')
@@ -37,9 +32,6 @@ txt_porta_selecionada_selec.grid(column=1, row=2)
 
 btn_selecionar = tk.Button(janela, text='Selecionar Porta', command = selec_port)
 btn_selecionar.grid(column=2, row=1)
-
-
-
 
 # combos
 minha_porta = tk.StringVar()
